bfbfd444e3a6919daf6a7827ba8ed978c65aa658
Jumper Chen <jumperchen@potix.com>
2013-11-27 03:51:12 +0000

    release 1.0.0 version

--- src/main/java/org/zkoss/addons/bootstrap/Version.java
+++ src/main/java/org/zkoss/addons/bootstrap/Version.java
@@ -3,5 +3,5 @@ package org.zkoss.addons.bootstrap;
 public class Version {
 	/** Returns the version UID.
 	 */
-	public static final String UID = "0.8.0-SNAPSHOT";
+	public static final String UID = "1.0.0";
 }
